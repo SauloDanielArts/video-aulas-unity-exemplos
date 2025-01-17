fb7411578e5cf945832ae2556b07adfe2d7addc3
Saulo Daniel Ferreira Pontes <saulodfp@gmail.com>
2025-01-17 10:21:12 -0300

    Adicionado controle das balas

--- BolasExplosivas/Assets/Scripts/Bala.cs
+++ BolasExplosivas/Assets/Scripts/Bala.cs
@@ -3,13 +3,21 @@ using UnityEngine;
 public class Bala : MonoBehaviour
 {
     public float forcaDoTiro = 10;
+    public Vector3 direcaoDoTiro;
+
+    public float tempoDeVida = 3f;
     void Start()
     {
-        GetComponent<Rigidbody>().AddForce(Vector3.forward * forcaDoTiro);
+        //GetComponent<Rigidbody>().AddForce(Vector3.forward * forcaDoTiro);
+        GetComponent<Rigidbody>().AddForce(direcaoDoTiro * forcaDoTiro);
     }
 
     void Update()
     {
-        
+        tempoDeVida -= Time.deltaTime;
+        if (tempoDeVida <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- BolasExplosivas/Assets/Scripts/Player.cs
+++ BolasExplosivas/Assets/Scripts/Player.cs
@@ -12,7 +12,20 @@ public class Player : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Instantiate(balaPrefab);
+           GameObject objetoBala = Instantiate(balaPrefab, transform.position, transform.rotation);
+           Bala bala = objetoBala.GetComponent<Bala>();
+               
+           //bala.direcaoDoTiro = Vector3.forward;
+           bala.direcaoDoTiro = transform.forward;
+           
+           //Vector3 direcaoDoTiro = bala.direcaoDoTiro;
+           
+            Vector3 direcaoDoTiro = new Vector3(
+               transform.position.x + Random.Range(-0.25f,0.25f),
+               transform.position.y + Random.Range(-0.2f,0.2f),
+               transform.position.z + 1);
+            
+            bala.transform.position = direcaoDoTiro;
         }
     }
 }
